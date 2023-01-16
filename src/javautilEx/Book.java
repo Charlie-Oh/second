f37729e0925fec2df40e76ecf5fbb287e2aa595a
hyunmin <hyunmin@192.168.200.165>
2023-01-16 21:52:56 +0900

    230116_javautilEx(Arrays,Calendar,clone,StringTokenizer)

---
+++ src/javautilEx/Book.java
@@ -0,0 +1,45 @@
+package javautilEx;
+
+public class Book implements Cloneable {
+	
+	public static void main(String[] args) {
+	}
+	
+	private String bookName;
+	private String author;
+	
+	public Book() {
+	}
+	
+	public Book(String bookName, String author) {
+		this.bookName=bookName;
+		this.author=author;
+	}
+	
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public void showBookInfo() {
+		System.out.println(bookName+","+author);
+	}
+	
+	public Object clone() {
+		Object o=null;
+		try {
+			o=super.clone();
+		} catch(CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return o;
+	}
+}
